ca21a7302e0e369ca18f61c0e600cf2ca0770478
Allan Souza <allanfermao@gmail.com>
2022-05-23 21:49:30 -0300

    Time scale added; multiple individuals; random initial point

--- My project/Assets/Animal.cs
+++ My project/Assets/Animal.cs
@@ -9,13 +9,24 @@ public class Animal : MonoBehaviour
     public LineRenderer lineRenderer;
     public List<Vector3> points;
     public int stepCount = 1;
+    public int timeScaleInDays;
+    public int coordTimeIntervalInMinutes;
+    public Dictionary<string, float> limits = new Dictionary<string, float>(){
+        {"L", -100f},
+        {"R", 100f},
+        {"T", 100f},
+        {"B", -100f}
+    };
 
     // Start is called before the first frame update
     void Start(){
+        float mu = 2.5f;
+        float x = Random.Range(limits["L"], limits["R"]);
+        float y = Random.Range(limits["B"], limits["T"]);     
 
-        int steps = 50;
-        float mu = 2f;
-        points = simm_levy(steps, mu, new Vector3(0, 0, -0.5f));
+        int steps = (int)(60 / coordTimeIntervalInMinutes) * 24 * timeScaleInDays; // times per hour * 24 hours * days
+
+        points = simm_levy(steps, mu, new Vector3(x, y, -0.5f));
 
         lineRenderer = GameObject.Find("LineRenderer").GetComponent<LineRenderer>();
         LineRenderer lineRendererC = Instantiate(lineRenderer);
@@ -45,9 +56,14 @@ public class Animal : MonoBehaviour
     }
 
     void FixedUpdate(){
-        float speed = 4f;
+        float speed = 50f;
         float step = speed * Time.fixedDeltaTime; // Set the step size
 
+        // TODO: avaliar a distância percorrida por um indivíduo durante um espaço de tempo criar um somatório 
+        // dos tamanhos de passos até atingir a distância total da simulação, ajustando a velocidade, sem pausas. 
+        // Para que todos os indivíduos terminem juntos, uma abordagem é parar quando o primeiro finalizar.
+        // outra abordagem é simular momentos de descanso e avaliar os valores de distância percorrida e tempo de atividade
+        // frequentemente. Com base nisso, ajustar a distância percorrida, obedecendo a velocidade padrão. 
 
         if(stepCount < points.Count){
             transform.position = Vector3.MoveTowards(transform.position, points[stepCount], step); // Move the individual gradually
@@ -61,8 +77,8 @@ public class Animal : MonoBehaviour
         float pi = 3.141592f;
         List<float> x = new List<float>();
         List<float> y = new List<float>();
-        x.Add(0);
-        y.Add(0);
+        x.Add(x0.x);
+        y.Add(x0.y);
 
         for (int i = 0; i < steps - 1; i++){
             float ang = (float)Random.Range(-pi, pi); // Get random angles (uniform distribuiton)
@@ -77,7 +93,7 @@ public class Animal : MonoBehaviour
         List<Vector3> points = new List<Vector3>();
         for (int i = 0; i < x.Count; i++){ // Create the Vector3 coordinates
             points.Add(new Vector3(x[i], y[i], -0.5f));
-            Debug.Log(x[i] + "; " + y[i]);
+            Debug.Log(i + ": " + x[i] + "; " + y[i]);
         }
 
         return points;
@@ -85,7 +101,7 @@ public class Animal : MonoBehaviour
 
     List<float> cumulative_sum(List<float> list){
         List<float> coord = new List<float>();
-        coord.Add(0);
+        coord.Add(list[0]);
         for (int i = 1; i < list.Count; i++){
             coord.Add(coord[i - 1] + list[i]);
         }
--- My project/Assets/Initializing.cs
+++ My project/Assets/Initializing.cs
@@ -4,18 +4,25 @@ using UnityEngine;
 
 public class Initializing : MonoBehaviour
 {
-
     public Sprite circle;
-    public GameObject circleElement;
+    public GameObject animal;
     public LineRenderer lineRenderer;
     public List<Vector3> points;
+    public int timeScaleInDays = 30;
+    public int coordTimeIntervalInMinutes = 30;
+    public int nAnimals = 3;
     public int stepCount = 1;
 
     // Start is called before the first frame update
     void Start(){
-        circleElement.AddComponent<Animal>();
 
-        GameObject animal = Instantiate(circleElement);        
+        Animal component = animal.AddComponent<Animal>();
+        component.timeScaleInDays = timeScaleInDays;
+        component.coordTimeIntervalInMinutes = coordTimeIntervalInMinutes;
+
+        for(int i=0; i < nAnimals-1; i++){
+            GameObject animalCopy = Instantiate(animal); 
+        }
     }
 
     // Update is called once per frame
